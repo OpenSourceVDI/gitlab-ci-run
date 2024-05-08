5cbec88355bd1c68c581494b6d4b97ff79266dd8
Rafael Gieschke <rafael@gieschke.de>
2024-05-08 18:02:15 +0200

    feat: allow to also execute (first) job

--- gitlab-ci-cat.py
+++ gitlab-ci-cat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import os
 import re
 import shlex
 import sys
@@ -16,12 +17,17 @@ parser.add_argument(
 parser.add_argument(
     "job", nargs="?", help="job to print; by default, all jobs are printed"
 )
+parser.add_argument("-e", "--exec", action="store_true", help="execute first found job")
+parser.add_argument(
+    "-r", "--runtime", default="podman", help="container runtime to execute the job"
+)
 args = parser.parse_args()
 
 gitlab_ci = yaml.safe_load(open(args.gitlab_ci_file))
 for name, job in gitlab_ci.items():
     if "script" in job:
         if args.job is None or name == args.job:
+            script = "\n".join(job["script"])
             print(f"# Job: {name}")
             # https://docs.gitlab.com/ee/ci/runners/hosted_runners/linux.html#container-images
             image = job.get("image") or gitlab_ci.get("image") or "ruby:3.1"
@@ -29,6 +35,8 @@ for name, job in gitlab_ci.items():
                 if not "/" in image:
                     image = f"library/{image}"
                 image = f"docker.io/{image}"
-            cmd = ["podman", "run", "-it", "-w", "/mnt", "-v", ".:/mnt", image]
+            cmd = [args.runtime, "run", "-it", "-w", "/mnt", "-v", ".:/mnt", image]
             print("#", shlex.join(cmd))
-            print("\n".join(job["script"]))
+            print(script)
+            if args.exec:
+                os.execvp(cmd[0], cmd + ["sh", "-c", script])
